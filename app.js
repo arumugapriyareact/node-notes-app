dc5b421e49cab8d036af25376de9d0012ac25d68
Arumuga priya <arumugapriya@smitiv.co>
2021-04-26 13:47:02 +0530

    chalk added

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const validator = require("validator");
+const chalk = require("chalk");
 const getNotes = require("./notes.js");
 
 const transport = getNotes();
 console.log(transport);
 console.log(validator.isEmail("am@app.com"));
+console.log(chalk.inverse.green("Success!"));
