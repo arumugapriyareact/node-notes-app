b290489b52d800004fceeaf43c1364cd6579a944
Arumuga priya <arumugapriya@smitiv.co>
2021-04-26 13:37:08 +0530

    validator added

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
+const validator = require("validator");
 const getNotes = require("./notes.js");
 
 const transport = getNotes();
 console.log(transport);
+console.log(validator.isEmail("am@app.com"));
